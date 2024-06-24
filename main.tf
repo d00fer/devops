898dbf9aaea339e37b9c4c80a41f7c156f8642f6
Patrycjusz Myszka <pat.myszka@gmail.com>
2024-06-24 23:00:31 +0100

    cc

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
   region = "eu-west-1"
 }
-resource "aws_instance" "example" {
+
+resource "aws_instance" "expample" {
   ami           = "ami-0f29c8402f8cce65c"
   instance_type = "t2.micro"
   tags = {
-    Name = "terraform-example"
+    Name = "example"
   }
 }
