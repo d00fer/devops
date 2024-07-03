5c76d97e26cdcf56b8e4e76d792346a5d016cf4c
Patrycjusz Myszka <pat.myszka@gmail.com>
2024-07-03 12:05:27 +0100

    cc

--- main.tf
+++ main.tf
@@ -9,7 +9,6 @@ output "public_ip" {
   value       = aws_instance.example.public_ip
   description = "Public ip of web server "
 }
-
 resource "aws_instance" "example" {
   ami                         = "ami-0f29c8402f8cce65c"
   instance_type               = "t2.micro"
